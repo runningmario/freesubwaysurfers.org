1e90a02cfd36599cb4b9d37376f5e9ba5f7cc32e
runningmario <144507514+runningmario@users.noreply.github.com>
2025-08-01 10:24:11 +0700

    Update poki-sdk.js

--- patch/js/poki-sdk.js
+++ patch/js/poki-sdk.js
@@ -145,14 +145,14 @@ PokiSDK= function() {
   this.commercialBreak= function(){
     consoleLog("--fx--PokiSDK--commercialBreak--");
     return new Promise((resolve, reject)=> {
-      loadJS("https://www.ubg235.com/ads/commercial.js", resolve);  
+      loadJS("https://subway-surfers.org//ads/commercial.js", resolve);  
     });
   }
 
   this.rewardedBreak= function() {
    consoleLog("--fx--PokiSDK--rewardedBreak--");
     return new Promise((resolve, reject)=> {
-      loadJS("https://www.ubg235.com/ads/rewarded.js", resolve);
+      loadJS("https://subway-surfers.org/ads/rewarded.js", resolve);
     }); 
   }
 
